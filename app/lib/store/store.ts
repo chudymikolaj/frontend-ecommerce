c884659bd22912174805c90a464f7815061709ba
chudymikolaj <chudymikolaj06@gmail.com>
2024-06-15 22:37:22 +0200

    feat: add redux and configureStore

---
+++ app/lib/store/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const shopStore = () => {
+	return configureStore({
+		reducer: {},
+	});
+};
+
+// Infer the type of shopStore
+export type AppStore = ReturnType<typeof shopStore>;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore["getState"]>;
+
+// Inferred type: {auth: authReducer}
+export type AppDispatch = AppStore["dispatch"];
