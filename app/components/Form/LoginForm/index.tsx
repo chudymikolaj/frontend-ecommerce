8d020672fc2c294a8f9835ec048d6328c37dbaac
chudymikolaj <chudymikolaj06@gmail.com>
2024-06-30 18:46:30 +0200

    feat: add LoginForm Component and shadow color variable

---
+++ app/components/Form/LoginForm/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { FormikHelpers } from "formik";
+import { signIn } from "next-auth/react";
+import * as Yup from "yup";
+
+import { ButtonSubmitForm, FormikForm, HeaderForm, InputForm, RecoverPassword } from "@components/Form";
+
+import styles from "./loginForm.module.scss";
+
+interface interfaceValues {
+	identifier: string;
+	password: string;
+}
+
+const SignupSchema = Yup.object().shape({
+	identifier: Yup.string().required("Podaj login lub e-mail"),
+	password: Yup.string().required("Wpisz hasło. Pole nie może być puste"),
+});
+
+const LoginForm = () => {
+	const formValue = {
+		identifier: "",
+		password: "",
+	};
+
+	const handleSubmit = async (values: interfaceValues, { setSubmitting }: FormikHelpers<interfaceValues>) => {
+		await signIn("credentials", {
+			...values,
+			callbackUrl: `${window.location.origin}/dashboard`,
+		});
+		setSubmitting(false);
+	};
+	return (
+		<div className={styles.LoginForm__container}>
+			<HeaderForm>Zaloguj się</HeaderForm>
+			<FormikForm
+				validationSchema={SignupSchema}
+				dataValues={formValue}
+				onSubmit={handleSubmit}
+			>
+				<InputForm
+					inputId="identifier"
+					placeholder="Wpisz login"
+					type="identifier"
+				/>
+				<InputForm
+					inputId="password"
+					placeholder="Wpisz hasło"
+					type="password"
+				/>
+				<RecoverPassword />
+				<ButtonSubmitForm buttonName="Zaloguj się" />
+			</FormikForm>
+		</div>
+	);
+};
+
+export default LoginForm;
---
+++ app/components/Form/index.tsx
@@ -0,0 +1,7 @@
+import FormikForm from "./Form";
+import HeaderForm from "./HeaderForm";
+import InputForm from "./Input";
+import ButtonSubmitForm from "./ButtonSubmitForm";
+import RecoverPassword from "./RecoverPassword";
+
+export { FormikForm, HeaderForm, InputForm, ButtonSubmitForm, RecoverPassword };
