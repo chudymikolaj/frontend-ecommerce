f48f6e1d3ab9edf504742df1fb69aa00f29da237
chudymikolaj <chudymikolaj06@gmail.com>
2024-06-30 18:38:01 +0200

    feat: add RecoverPassword component

---
+++ app/components/Form/RecoverPassword/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./recoverPassword.module.scss";
+
+const RecoverPassword = () => {
+	return (
+		<a
+			href="#"
+			className={styles.RecoverPassword__button}
+		>
+			Nie pamiętasz hasła?
+		</a>
+	);
+};
+
+export default RecoverPassword;
