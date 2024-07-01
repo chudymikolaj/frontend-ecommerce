3975efb923ca9ee09c710a2a2c853a5106f59eb7
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-01 23:39:35 +0200

    feat: add RegisterForm component

---
+++ app/components/Form/RegisterForm/index.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { FormikHelpers } from "formik";
+import { signIn } from "next-auth/react";
+import * as Yup from "yup";
+
+import { ButtonSubmitForm, FormikForm, HeaderForm, InputForm, RedirectForm } from "@components/Form";
+import { register } from "@services/authService";
+
+import styles from "./registerForm.module.scss";
+
+interface interfaceValues {
+	username: string;
+	email: string;
+	password: string;
+}
+
+const SignupSchema = Yup.object().shape({
+	username: Yup.string().min(3).required("Podaj nazwę konta. Powinno składać się minimum z 3 znaków."),
+	email: Yup.string().email().min(6).required("Podaj e-mail. Powinno składać się minimum z 6 znaków."),
+	password: Yup.string().min(8).required("Wpisz hasło. Powinno składać się minimum z 8 znaków."),
+});
+
+const RegisterForm = () => {
+	const formValue = {
+		username: "",
+		email: "",
+		password: "",
+	};
+
+	const handleSubmit = async (values: interfaceValues, { setSubmitting }: FormikHelpers<interfaceValues>) => {
+		await register(values.username, values.email, values.password);
+		await signIn("credentials", {
+			identifier: values.email,
+			password: values.password,
+			callbackUrl: `${window.location.origin}/`,
+		});
+
+		setSubmitting(false);
+	};
+	return (
+		<div className={styles.RegisterForm__container}>
+			<HeaderForm>Rejestracja użytkownika</HeaderForm>
+			<FormikForm
+				validationSchema={SignupSchema}
+				dataValues={formValue}
+				onSubmit={handleSubmit}
+			>
+				<InputForm
+					inputId="username"
+					placeholder="Nazwa konta (wymaganae)"
+					type="text"
+				/>
+				<InputForm
+					inputId="email"
+					placeholder="E-mail (wymaganae)"
+					type="email"
+				/>
+				<InputForm
+					inputId="password"
+					placeholder="Hasło (wymaganae)"
+					type="password"
+				/>
+				<ButtonSubmitForm buttonName="Załóż konto" />
+			</FormikForm>
+			<RedirectForm
+				url="/login"
+				text="Masz już konto?"
+				name="Zaloguj się"
+			/>
+		</div>
+	);
+};
+
+export default RegisterForm;
