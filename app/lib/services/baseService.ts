c822adc5fa406dabc6aacba4d046adc6a1f2865c
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-05 21:15:43 +0200

    refactore: change endpoint parameters in navbarRequest

--- app/lib/services/baseService.ts
+++ app/lib/services/baseService.ts
@@ -2,7 +2,9 @@ import { axiosGetRequest } from "./axiosRequest";
 
 const navbarRequest = async () => {
 	try {
-		const response = await axiosGetRequest("/api/navbar?populate[0]=LogotypeName,Menu&populate[1]=MenuButtons.Icon");
+		const response = await axiosGetRequest(
+			"/api/navbar?populate[0]=LogotypeName,Menu&populate[1]=UserLinks.UserLink.Icon,UserLinks.UserLinks.Icon"
+		);
 
 		return response.data;
 	} catch (error) {
