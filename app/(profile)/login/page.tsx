191bfcc6e34d6e728c24266457af26324021fcdc
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-08 22:20:00 +0200

    feat: add redirection to registration and login pages

--- app/(profile)/login/page.tsx
+++ app/(profile)/login/page.tsx
@@ -1,8 +1,11 @@
 import LoginForm from "@components/Form/LoginForm";
+import RedirectToDashboard from "@services/authRedirect";
 
 import styles from "./login.module.scss";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+	await RedirectToDashboard();
+
 	return (
 		<main className={styles.LoginPage_container}>
 			<LoginForm />
--- app/(profile)/register/page.tsx
+++ app/(profile)/register/page.tsx
@@ -1,8 +1,11 @@
 import RegisterForm from "@components/Form/RegisterForm";
+import RedirectToDashboard from "@services/authRedirect";
 
 import styles from "./register.module.scss";
 
-const Register = () => {
+const Register = async () => {
+	await RedirectToDashboard();
+
 	return (
 		<main className={styles.RegisterPage_container}>
 			<RegisterForm />
