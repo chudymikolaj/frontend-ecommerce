36119e16d5e5e4efefe2dde6e76e474fb1b24c89
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-16 22:26:55 +0200

    refactore: changing the structure and add list of products in the DropdownCart Component

--- app/components/Navbar/Cart/DropdownCart/index.tsx
+++ app/components/Navbar/Cart/DropdownCart/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import { useAppSelector } from "@store/hooks";
+import PrimaryButton from "@components/PrimaryButton";
 
 import styles from "./dropdownCart.module.scss";
 
@@ -6,13 +9,58 @@ type DropdownCart = {
 	className: string;
 };
 
+const CMS_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+
 const DropdownCart = ({ className }: DropdownCart) => {
+	const { items: itemsCart, productCount } = useAppSelector((state) => state.cart);
+	const isCart = productCount > 0;
+
 	return (
 		<div className={`${styles.DropdownCart__container} ${className}`}>
-			<div className={styles.DropdownCart__container_products}>
-				<div className={styles.DropdownCart__container_products_empty}>
-					<p className={styles.DropdownCart__container_products_empty_text}>Twój koszyk jest pusty</p>
-				</div>
+			<div className={styles.DropdownCart__container_cart}>
+				{!isCart && (
+					<div className={styles.DropdownCart__container_products_empty}>
+						<p className={styles.DropdownCart__container_products_empty_text}>Twój koszyk jest pusty</p>
+						<PrimaryButton
+							linkTo="/"
+							name="Przejdź do strony głównej"
+						/>
+					</div>
+				)}
+
+				{isCart && (
+					<>
+						<div className={styles.DropdownCart__container_title}>
+							Koszyk (<span>{productCount}</span>)
+						</div>
+						<div className={styles.DropdownCart__container_products}>
+							{itemsCart.map((items, index) => {
+								return (
+									<div
+										key={index}
+										className={styles.DropdownCart__container_products__item}
+									>
+										<img
+											src={`${CMS_URL}${items.image.data.attributes.url}`}
+											alt={`${CMS_URL}${items.image.data.attributes.alternativeText}`}
+											className={styles.DropdownCart__container_products__item_image}
+										/>
+										<div className={styles.DropdownCart__container_products__item_text}>
+											<span>{items.name}</span>
+											<span>{items.quantity} szt.</span>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+						<div className={styles.DropdownCart__container_button}>
+							<PrimaryButton
+								linkTo="/cart"
+								name="Przejdź do koszyka"
+							/>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
