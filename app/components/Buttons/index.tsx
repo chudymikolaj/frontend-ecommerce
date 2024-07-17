57c3a4422f00367f128994297c4996c8e303676d
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-17 16:10:30 +0200

    refactore: rename the component folder to Buttons and add the BorderButton component

---
+++ app/components/Buttons/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+import styles from "./buttons.module.scss";
+
+type PrimaryButtonPropsType = {
+	linkTo: string;
+	name: string;
+};
+
+export const PrimaryButton = ({ linkTo, name }: PrimaryButtonPropsType) => {
+	return (
+		<Link
+			href={linkTo}
+			className={styles.PrimaryButton}
+		>
+			{name}
+		</Link>
+	);
+};
+
+export const BorderButton = ({ linkTo, name }: PrimaryButtonPropsType) => {
+	return (
+		<Link
+			href={linkTo}
+			className={styles.BorderButton}
+		>
+			{name}
+		</Link>
+	);
+};
