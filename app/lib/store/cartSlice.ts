9fa20f84c5bdc0141eda5fd104340806c8d37441
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-26 12:01:31 +0200

    refactore: add window type for localStorage, add download cart from CMS and add new items to cart or change Quantity and change downloaded image object to numeric id for cms

--- app/lib/store/cartSlice.ts
+++ app/lib/store/cartSlice.ts
@@ -4,12 +4,24 @@ import { createAsyncThunk, createSlice, nanoid, PayloadAction } from "@reduxjs/t
 import type {
 	CartStateInterface,
 	PostProductToOrderCollectionResponse,
+	GetProductToOrderCollectionResponse,
 	PostProductToOrderCollectionProps,
 	PostProductToOrderCollectionFulfilledType,
+	CartItemUpdate,
+	CartItem,
 } from "./store.types";
 
+const getBasketSyncToken = (): string | null => {
+	if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
+		return localStorage.getItem("basketSync");
+	}
+	return null;
+};
+
+const basketSyncToken = getBasketSyncToken();
+
 const initialState: CartStateInterface = {
-	basketSync: localStorage.getItem("basketSync") ? localStorage.getItem("basketSync") : null,
+	basketSync: basketSyncToken,
 	items: [],
 	productCount: 0,
 	loading: "idle",
@@ -20,36 +32,112 @@ const initialState: CartStateInterface = {
 // Thunk action for posting cart items
 export const postProductToOrderCollection = createAsyncThunk(
 	"cart/postProductToOrderCollectionStatus",
-	async (item: PostProductToOrderCollectionProps, { rejectWithValue, extra }) => {
+	async (item: PostProductToOrderCollectionProps, { rejectWithValue, extra, getState }) => {
 		const config = extra as { apiUrl: string };
+		const state = getState() as { cart: CartStateInterface };
+		const basketSyncToken = state.cart.basketSync;
 
 		try {
 			const sendItem = {
 				...item,
-				Image: item.Image.data.id,
-			};
-
-			const cartItems = {
-				data: { BasketToken: nanoid(), Products: [sendItem] },
+				Image: item.Image.data.id, // Use the image id directly
 			};
 
 			const headers = {
 				Authorization: `Bearer ${process.env.NEXT_PUBLIC_STORE_CART_ORDERS}`,
 			};
 
-			const response = await axios.post<PostProductToOrderCollectionResponse>(
-				`${config.apiUrl}/api/carts?populate[Products][populate]=*&populate[BasketToken][populate]=*`,
-				cartItems,
-				{
-					headers,
+			let response;
+
+			if (basketSyncToken) {
+				// Fetch existing cart data
+				const cartResponse = await axios.get<GetProductToOrderCollectionResponse>(
+					`${config.apiUrl}/api/carts?filters[BasketToken][$eq]=${basketSyncToken}&populate[Products][populate]=*&populate[BasketToken][populate]=*`,
+					{ headers }
+				);
+
+				const existingCart = cartResponse.data.data[0];
+				const existingProducts = existingCart.attributes.Products.map((product) => ({
+					...product,
+					Image: product.Image.data?.id || product.Image, // Ensure image is an id
+				}));
+
+				const existingProduct = existingProducts.find((product) => product.IdProduct === item.IdProduct);
+
+				let updatedCartItems;
+
+				if (existingProduct) {
+					// Update the quantity of the existing product
+					existingProduct.Quantity += item.Quantity;
+					existingProduct.Image = item.Image.data.id; // Update image id
+
+					let updatedArray = existingProducts.map((product) =>
+						product.IdProduct === existingProduct.IdProduct ? existingProduct : product
+					);
+
+					updatedCartItems = {
+						data: {
+							Products: updatedArray,
+						},
+					};
+				} else {
+					// Add new item to the existing cart items
+					updatedCartItems = {
+						data: {
+							Products: [...existingProducts, sendItem],
+						},
+					};
 				}
-			);
 
-			const getBasketToken = response.data.data.attributes.BasketToken;
+				// Update the existing cart
+				response = await axios.put<PostProductToOrderCollectionResponse>(
+					`${config.apiUrl}/api/carts/${existingCart.id}?populate[Products][populate]=*&populate[BasketToken][populate]=*`,
+					updatedCartItems,
+					{ headers }
+				);
+			} else {
+				// Create new cart
+				const newCartItems = {
+					data: { BasketToken: nanoid(), Products: [sendItem] },
+				};
 
-			localStorage.setItem("basketSync", getBasketToken);
+				response = await axios.post<PostProductToOrderCollectionResponse>(
+					`${config.apiUrl}/api/carts?populate[Products][populate]=*&populate[BasketToken][populate]=*`,
+					newCartItems,
+					{ headers }
+				);
+
+				const getBasketToken = response.data.data.attributes.BasketToken;
+				localStorage.setItem("basketSync", getBasketToken);
+			}
+
 			return response.data.data.attributes;
 		} catch (error: any) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
+
+export const getProductsFromOrderCollectionToCart = createAsyncThunk(
+	"cart/getProductsFromOrderCollectionToCart",
+	async (BasketToken: string | null, { rejectWithValue, extra }) => {
+		const config = extra as { apiUrl: string };
+
+		if (BasketToken === null) return;
+
+		try {
+			const headers = {
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_STORE_CART_ORDERS}`,
+			};
+
+			const response = await axios.get<GetProductToOrderCollectionResponse>(
+				`${config.apiUrl}/api/carts?filters[BasketToken][$eq]=${BasketToken}&populate[Products][populate]=*&populate[BasketToken][populate]=*`,
+				{ headers }
+			);
+
+			return response.data.data;
+		} catch (error: any) {
+			localStorage.removeItem("basketSync");
 			return rejectWithValue(error.message);
 		}
 	}
@@ -60,18 +148,6 @@ const cartSlice = createSlice({
 	name: "cart",
 	initialState,
 	reducers: {
-		// addToCart: (state, action: PayloadAction<PostProductToOrderCollectionProps>) => {
-		// 	const item = action.payload;
-		// 	const existingItem = state.items.find((i) => i.IdProduct === item.IdProduct);
-
-		// 	if (existingItem) {
-		// 		existingItem.Quantity += item.Quantity;
-		// 	} else {
-		// 		state.items.push({ ...item });
-		// 	}
-
-		// 	state.productCount += item.Quantity;
-		// },
 		removeFromCart: (state, action: PayloadAction<{ IdProduct: string }>) => {
 			const itemId = action.payload.IdProduct;
 			const item = state.items.find((i) => i.IdProduct === itemId);
@@ -88,6 +164,26 @@ const cartSlice = createSlice({
 	},
 	extraReducers: (builder) => {
 		builder
+			.addCase(getProductsFromOrderCollectionToCart.fulfilled, (state, action) => {
+				state.loading = "succeeded";
+				let items = action.payload;
+
+				if (items != null && items[0]) {
+					state.items = items[0]?.attributes.Products;
+					state.basketSync = items[0]?.attributes.BasketToken;
+				} else {
+					state.loading = "idle";
+					state.basketSync = null;
+					localStorage.removeItem("basketSync");
+				}
+
+				state.productCount = state.items.reduce((count, item) => count + item.Quantity, 0);
+			})
+			.addCase(getProductsFromOrderCollectionToCart.rejected, (state, action) => {
+				state.loading = "failed";
+				localStorage.removeItem("basketSync");
+			});
+		builder
 			.addCase(postProductToOrderCollection.pending, (state) => {
 				state.loading = "pending";
 			})
@@ -95,15 +191,15 @@ const cartSlice = createSlice({
 				postProductToOrderCollection.fulfilled,
 				(state, action: PayloadAction<PostProductToOrderCollectionFulfilledType>) => {
 					state.loading = "succeeded";
-
 					const items = action.payload;
-					console.log(items);
-					const existingItemsMap = new Map(state.items.map((item) => [item.IdProduct, item]));
 
+					const existingItemsMap = new Map(state.items.map((item) => [item.IdProduct, item]));
 					items.Products.forEach((item) => {
 						const existingItem = existingItemsMap.get(item.IdProduct);
+
 						if (existingItem) {
-							existingItem.Quantity += item.Quantity;
+							existingItem.Quantity = item.Quantity;
+							existingItem.Image = item.Image; // Ensure the image id field is updated
 						} else {
 							state.items.push(item);
 						}
--- app/lib/store/store.types.ts
+++ app/lib/store/store.types.ts
@@ -1,42 +1,72 @@
 import { SerializedError } from "@reduxjs/toolkit";
-import { ProductImageType } from "../services/baseService.types";
+import { ProductImageType } from "@services/baseService.types";
 
-export interface CartItem {
+export type CartItem = {
 	IdProduct: string;
 	Slug: string;
 	Name: string;
 	Price: number;
-	Image: ProductImageType;
+	Image: ProductImageType; // Image can be an object with an id or just a number
 	Quantity: number;
-}
+};
 
-export interface CartStateInterface {
-	basketSync: string | null | undefined;
+export type CartItemUpdate = {
+	IdProduct: string;
+	Slug: string;
+	Name: string;
+	Price: number;
+	Image: { data: { id: number } }; // Image as an id for updates
+	Quantity: number;
+};
+
+export type CartStateInterface = {
+	basketSync: string | null;
 	items: CartItem[];
 	productCount: number;
 	loading: "idle" | "pending" | "succeeded" | "failed";
 	error: SerializedError | string | null;
 	currentRequestCartId?: string;
-}
+};
 
-export interface PostProductToOrderCollectionResponse {
+export type GetProductToOrderCollectionResponse = {
+	data: [
+		{
+			id: number;
+			attributes: {
+				BasketToken: string;
+				Products: CartItemUpdate[] & CartItem[];
+			};
+		}
+	];
+};
+
+export type GetProductToOrderCollectionBuilderResponse = {
+	id: number;
+	attributes: {
+		BasketToken: string;
+		Products: CartItem[];
+	};
+};
+
+export type PostProductToOrderCollectionResponse = {
 	data: {
 		attributes: {
 			BasketToken: string;
 			Products: CartItem[];
 		};
 	};
-}
-export interface PostProductToOrderCollectionFulfilledType {
+};
+
+export type PostProductToOrderCollectionFulfilledType = {
 	BasketToken: string;
 	Products: CartItem[];
-}
+};
 
-export interface PostProductToOrderCollectionProps {
+export type PostProductToOrderCollectionProps = {
 	IdProduct: string;
 	Slug: string;
 	Name: string;
 	Price: number;
-	Image: { data: { id: number } };
+	Image: { data: { id: number } }; // Image as object with id for posting
 	Quantity: number;
-}
+};
