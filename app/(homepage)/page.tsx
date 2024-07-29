06f46dea74b0dc69dc0260883c402ed340a6b2b6
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-29 22:16:34 +0200

    refactore: rebuild homepage, add ShowcaseMainPage element and fetch data from CMS

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,9 +1,40 @@
-import styles from "./page.module.scss";
+import { notFound } from "next/navigation";
+import { axiosGetOnePageRequest } from "@services/axiosRequest";
+import ImageComponent from "@components/Image";
 
-export default function Home() {
+import styles from "./homepage.module.scss";
+import { CTAButton } from "../components/Buttons";
+
+const HomePage = async () => {
+	const result = await axiosGetOnePageRequest("/homepage", "&populate[ShowcaseMainPage][populate]=*");
+
+	if (result.length === 0) return notFound;
+
+	const getHomepageAttributes = result[0].attributes;
+	const { ShowcaseMainPage } = getHomepageAttributes;
+	const { Header, HeaderImage, DescriptionWithImage } = ShowcaseMainPage;
+	const { Header: TextHeader, Text, ButtonName, ButtonUrl } = DescriptionWithImage;
+
 	return (
-		<main className={styles.main}>
-			<div className={styles.description}></div>
+		<main className={styles.Homepage__container}>
+			<div className={styles.Homepage__container__ShowcaseMainPage}>
+				<div className={styles.Homepage__container__ShowcaseMainPage__imageWrapper}>
+					<h1 className={styles.Homepage__container__ShowcaseMainPage__imageWrapper_title}>{Header}</h1>
+					<div className={styles.Homepage__container__ShowcaseMainPage__imageWrapper_image}>
+						<ImageComponent data={HeaderImage} />
+					</div>
+				</div>
+				<div className={styles.Homepage__container__ShowcaseMainPage__descriptionAction}>
+					<h2 className={styles.Homepage__container__ShowcaseMainPage__descriptionAction_title}>{TextHeader}</h2>
+					<p className={styles.Homepage__container__ShowcaseMainPage__descriptionAction_text}>{Text}</p>
+					<CTAButton
+						name={ButtonName}
+						linkTo={ButtonUrl}
+					/>
+				</div>
+			</div>
 		</main>
 	);
-}
+};
+
+export default HomePage;
