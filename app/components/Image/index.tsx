6fb77d66beb31598157260ec8a7d1485c98b250d
chudymikolaj <chudymikolaj06@gmail.com>
2024-07-31 22:19:32 +0200

    refactore: change type props in Image Component

--- app/components/Image/index.tsx
+++ app/components/Image/index.tsx
@@ -1,3 +1,4 @@
+import type { ProductImageType } from "@/app/lib/services/baseService.types";
 import Image from "next/image";
 
 import React from "react";
@@ -5,29 +6,7 @@ import React from "react";
 const CMS_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 type ImageComponentProps = {
-	data: {
-		data: {
-			id: number;
-			attributes: {
-				name: string;
-				alternativeText: string | null;
-				caption: string | null;
-				width: number;
-				height: number;
-				formats: [Object];
-				hash: string;
-				ext: ".png";
-				mime: "image/png";
-				size: number;
-				url: string;
-				previewUrl: string | null;
-				provider: "local";
-				provider_metadata: null;
-				createdAt: string;
-				updatedAt: string;
-			};
-		};
-	};
+	data: ProductImageType;
 };
 
 const ImageComponent = ({ data }: ImageComponentProps) => {
