88d79e2b7ae2c2fd9ff9fc598a224b3c465064b8
chudymikolaj <chudymikolaj06@gmail.com>
2024-08-11 22:09:54 +0200

    refactore: add svg import from public folder, change to svg component and rename to AddToCartButtonProductsSectionItem component

--- app/components/Product/AddToCartButton/index.tsx
+++ app/components/Product/AddToCartButton/index.tsx
@@ -2,12 +2,13 @@
 
 import { postProductToOrderCollection } from "@store/cartSlice";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { useState } from "react";
 
 import Quantity from "@components/Product/Quantity";
 
-import { useState } from "react";
 import styles from "./addToCartButton.module.scss";
 import AddToCartButtonProps from "./addToCartButton.types";
+import ProgressActivityIcon from "public/progress_activity.svg";
 
 const AddToCartButton = ({ attributes }: AddToCartButtonProps) => {
 	const { idProduct, name, slug, price, count, image } = attributes;
@@ -46,20 +47,7 @@ const AddToCartButton = ({ attributes }: AddToCartButtonProps) => {
 			>
 				{isLoading ? (
 					<div className={styles.spinner__container}>
-						<svg
-							className={styles.spinner__container__svg}
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-								opacity=".25"
-							/>
-							<path
-								d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
-								className={styles.spinner__container__svg_icon}
-							/>
-						</svg>
+						<ProgressActivityIcon className={styles.spinner__container__svg} />
 					</div>
 				) : null}
 				Dodaj do koszyka
