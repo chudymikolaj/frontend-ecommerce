148642d7840d73c7c85239d1040f68c1bbb65c02
chudymikolaj <chudymikolaj06@gmail.com>
2024-08-27 23:11:12 +0200

    feat: add data fetch from cms and style BentoSection component

--- app/components/Sections/BentoSection/bentoSection.types.ts
+++ app/components/Sections/BentoSection/bentoSection.types.ts
@@ -4,20 +4,24 @@ export type BentoSectionType = {
 	id: number;
 	__component: "sections.bento-section";
 	FirstBentoItemName: string;
-	SecondBentoItemName: string;
-	BentoSectionDescription: string;
-	FirstBentoItem: PageSectionsImageType;
+	FirstBentoItemImage: PageSectionsImageType;
 	FirstBentoItemButton: {
 		id: number;
 		Name: string;
 		Url: string;
 	};
+	FirstBentoItemBackgroundColor: string;
+	FirstBentoItemBackgroundImage: PageSectionsImageType;
+	SecondBentoItemName: string;
 	SecondBentoItemImage: PageSectionsImageType;
 	SecondBentoItemButton: {
 		id: number;
 		Name: string;
 		Url: string;
 	};
+	SecondBentoItemBackgroundColor: string;
+	SecondBentoItemBackgroundImage: PageSectionsImageType;
+	BentoSectionDescription: string;
 };
 
 export type BentoSectionPropsType = {
--- app/components/Sections/BentoSection/index.tsx
+++ app/components/Sections/BentoSection/index.tsx
@@ -1,15 +1,82 @@
-import React from "react";
+import Link from "next/link";
+import ImageComponent from "@components/Image";
+
 import type { BentoSectionPropsType } from "./bentoSection.types";
+import styles from "./bentoSection.module.scss";
+
+const CMS_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 const BentoSection = ({ element }: BentoSectionPropsType) => {
-	// console.log(element);
+	const {
+		FirstBentoItemName,
+		FirstBentoItemButton,
+		FirstBentoItemImage,
+		FirstBentoItemBackgroundImage,
+		FirstBentoItemBackgroundColor,
+		SecondBentoItemName,
+		SecondBentoItemButton,
+		SecondBentoItemImage,
+		SecondBentoItemBackgroundColor,
+		SecondBentoItemBackgroundImage,
+		BentoSectionDescription,
+	} = element;
+
+	const isFirstBentoBackground = `url('${CMS_URL}${FirstBentoItemBackgroundImage.data?.attributes.url}')`;
+	const isFirstBentoColor = FirstBentoItemBackgroundColor;
+	const isSecondBentoBackground = `url('${CMS_URL}${SecondBentoItemBackgroundImage.data?.attributes.url}')`;
+	const isSecondBentoColor = SecondBentoItemBackgroundColor;
+
 	return (
-		<div>
-			<div>
-				<div>Fisrt banner</div>
-				<div>text</div>
+		<div className={styles.BentoSection__container}>
+			<div className={styles.BentoSection__container__wrapper}>
+				{FirstBentoItemImage && (
+					<div
+						className={styles.BentoSection__container__firstBento_item}
+						style={{ backgroundColor: isFirstBentoColor, backgroundImage: isFirstBentoBackground }}
+					>
+						<div>
+							{FirstBentoItemName && (
+								<h3 className={styles.BentoSection__container__item_title}>{FirstBentoItemName}</h3>
+							)}
+							{FirstBentoItemButton && (
+								<Link
+									href={FirstBentoItemButton.Url}
+									className={styles.BentoSection__container__item_button}
+								>
+									{FirstBentoItemButton.Name}
+								</Link>
+							)}
+						</div>
+						<ImageComponent
+							data={FirstBentoItemImage}
+							className={styles.BentoSection__container__firstBento__item_image}
+						/>
+					</div>
+				)}
+				{BentoSectionDescription && (
+					<div className={styles.BentoSection__container__wrapper_text}>{BentoSectionDescription}</div>
+				)}
 			</div>
-			<div>Secound banner</div>
+			<div
+				className={styles.BentoSection__container__lastBento_item}
+				style={{ backgroundColor: isSecondBentoColor, backgroundImage: isSecondBentoBackground }}
+			>
+				<div>
+					{SecondBentoItemName && <h3 className={styles.BentoSection__container__item_title}>{SecondBentoItemName}</h3>}
+					{SecondBentoItemButton && (
+						<Link
+							href={SecondBentoItemButton.Url}
+							className={styles.BentoSection__container__item_button}
+						>
+							{SecondBentoItemButton.Name}
+						</Link>
+					)}
+				</div>
+				<ImageComponent
+					data={SecondBentoItemImage}
+					className={styles.BentoSection__container__lastBento__item_image}
+				/>
+			</div>
 		</div>
 	);
 };
