54280d54d3e3c332d1aaab2ccf4cc79c9bc1e3f2
chudymikolaj <chudymikolaj06@gmail.com>
2024-11-05 23:14:40 +0100

    refactore: prepare cart page, styles and some simple functions

--- app/(cart)/cart/page.tsx
+++ app/(cart)/cart/page.tsx
@@ -5,17 +5,20 @@ import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { removeFromCart, clearCart, getProductsFromOrderCollectionToCart } from "@store/cartSlice";
 import { InfoButton } from "@components/Buttons";
-// import { submitCart } from "../features/cart/cartThunks";
 
+import { ActionButton, SubmitButton } from "@components/Buttons";
 import styles from "./cart.module.scss";
+import ImageComponent from "@/app/components/Image";
+import Currency from "@/app/lib/currency";
 
 const CartPage = () => {
 	const pathname = usePathname();
 	const dispatch = useAppDispatch();
 	const { items: cartItems, productCount, loading, basketSync } = useAppSelector((state) => state.cart);
-	// const status = useSelector((state) => state.cart.status);
-	// const error = useSelector((state) => state.cart.error);
+	const error = useAppSelector((state) => state.cart.error);
 	const isProducts = productCount > 0;
+	const isLodingProdutsInCart = isProducts && loading === "succeeded";
+	const isLodingAndEmptyCart = !isProducts && loading === "succeeded";
 
 	useEffect(() => {
 		if (loading === "idle") {
@@ -23,7 +26,7 @@ const CartPage = () => {
 		}
 	}, [loading, dispatch]);
 
-	const handleRemoveItem = (IdProduct) => {
+	const handleRemoveItem = (IdProduct: string) => {
 		dispatch(removeFromCart({ IdProduct }));
 	};
 
@@ -31,48 +34,140 @@ const CartPage = () => {
 		dispatch(clearCart());
 	};
 
-	// const handleSubmitCart = () => {
-	// 	dispatch(submitCart(cartItems));
-	// };
+	const handleSubmitCart = () => {
+		// dispatch(submitCart(cartItems));
+		dispatch(clearCart());
+	};
 
 	return (
-		<div>
-			{!isProducts && (
-				<div className={styles.EmptyCart__container}>
-					<p className={styles.EmptyCart__container_text}>Twój koszyk jest pusty</p>
-					{pathname !== "/" && (
-						<InfoButton
-							linkTo="/"
-							name="Przejdź do strony głównej"
-						/>
-					)}
-				</div>
-			)}
+		<div className={styles.CartPage}>
+			{/* <div className={styles.CartPage_breadcrumbs}>
+				<span>Koszyk</span> | <span>Dostawa i płatność </span> | <span>Podsumowanie</span>
+			</div> */}
 
-			{isProducts && (
-				<>
-					<h2>Cart</h2>
-					{/* {status === "loading" && <p>Submitting...</p>}
-					{status === "failed" && <p>Error: {error}</p>} */}
-					<ul>
-						{cartItems.map((item) => (
-							<li key={item.IdProduct}>
-								{item.Name} - {item.Quantity}
-								<button
-									onClick={() => {
-										handleRemoveItem(item.IdProduct);
-										console.log(item.IdProduct);
-									}}
-								>
-									Remove
-								</button>
-							</li>
-						))}
-					</ul>
-					<button onClick={handleClearCart}>Clear Cart</button>
-					{/* <button onClick={handleSubmitCart}>Submit Cart</button> */}
-				</>
-			)}
+			<div className={styles.CartPage__container}>
+				{loading === "idle" && <p>Submitting...</p>}
+				{loading === "failed" && <p>Error: {error?.toString()}</p>}
+
+				{isLodingAndEmptyCart && (
+					<div className={styles.EmptyCart__container}>
+						<p className={styles.EmptyCart__container_text}>Twój koszyk jest pusty</p>
+						{pathname !== "/" && (
+							<InfoButton
+								linkTo="/"
+								name="Przejdź do strony głównej"
+							/>
+						)}
+					</div>
+				)}
+
+				{isLodingProdutsInCart && (
+					<>
+						<div className={styles.CartPage__container_header}>
+							<h1 className={styles.CartPage__container_header__title}>Koszyk(1)</h1>
+						</div>
+
+						<div className={styles.CartPage__grid}>
+							<div className={styles.CartPage__grid__wrapper}>
+								<ul className={styles.CartPage__grid__productList}>
+									{cartItems.map((item) => {
+										console.log(item);
+										return (
+											<li
+												className={styles.CartPage__grid__productList_product}
+												key={item.IdProduct}
+											>
+												<ImageComponent
+													data={item.Image}
+													className={styles.CartPage__grid__productList_product_image}
+												/>
+												<div className={styles.CartPage__grid__productList_product_wrapper}>
+													<div className={styles.CartPage__grid__productList_product_wrapper__row}>
+														<div>
+															<p className={styles.CartPage__grid__productList_product_wrapper__row_name}>
+																{item.Name}
+															</p>
+															<p className={styles.CartPage__grid__productList_product_wrapper__row_color}>Kolor: -</p>
+														</div>
+														<p className={styles.CartPage__grid__productList_product_wrapper__row_price}>
+															Cena <Currency price={String(item.Price)} />
+														</p>
+													</div>
+
+													<div className={styles.CartPage__grid__productList_product_wrapper__row}>
+														<p>Ilość: {item.Quantity}</p>
+														<div className={styles.CartPage__grid__productList_product_wrapper__row_buttons}>
+															<button
+																className={styles.CartPage__grid__productList_product_wrapper__row_button}
+																onClick={() => {
+																	console.log(item.IdProduct);
+																}}
+															>
+																Zmień
+															</button>
+															<button
+																className={styles.CartPage__grid__productList_product_wrapper__row_button}
+																onClick={() => {
+																	handleRemoveItem(item.IdProduct);
+																	console.log(item.IdProduct);
+																}}
+															>
+																Usuń
+															</button>
+														</div>
+													</div>
+												</div>
+											</li>
+										);
+									})}
+								</ul>
+
+								<ActionButton action={handleClearCart}>Clear Cart</ActionButton>
+							</div>
+
+							<div className={styles.CartPage__grid__cart_summary}>
+								<div className={styles.CartPage__grid__cart_summary_border}>
+									<div className={styles.CartPage__grid__cart_summary__before_total}>
+										<h2 className={styles.CartPage__grid__cart_summary__before_total_title}>Podsumowanie zamówienia</h2>
+
+										<div className={styles.CartPage__grid__cart_summary__before_total__wrapper}>
+											<div className={styles.CartPage__grid__cart_summary__before_total__row}>
+												<p className={styles.CartPage__grid__cart_summary__before_total__row_name}>Suma częściowa</p>
+												<span className={styles.CartPage__grid__cart_summary__before_total__row_price}>6299,00 zł</span>
+											</div>
+											<div className={styles.CartPage__grid__cart_summary__before_total__row}>
+												<p className={styles.CartPage__grid__cart_summary__before_total__row_name}>
+													Dostawa i płatnośc
+												</p>
+												<span className={styles.CartPage__grid__cart_summary__before_total__row_price}>0 zł</span>
+											</div>
+											<div className={styles.CartPage__grid__cart_summary__before_total__row}>
+												<p className={styles.CartPage__grid__cart_summary__before_total__row_name}>Podatek VAT</p>
+												<span className={styles.CartPage__grid__cart_summary__before_total__row_price}>
+													Uwzględniono
+												</span>
+											</div>
+										</div>
+									</div>
+
+									<div className={styles.CartPage__grid__cart_summary__total}>
+										<hr />
+
+										<div className={styles.CartPage__grid__cart_summary__total__row__wrapper}>
+											<div className={styles.CartPage__grid__cart_summary__total__row}>
+												<p className={styles.CartPage__grid__cart_summary__total__row_name}>Szacowana suma</p>
+												<span className={styles.CartPage__grid__cart_summary__total__row_price}>6299,00 zł</span>
+											</div>
+										</div>
+
+										<SubmitButton action={handleSubmitCart}>Submit Cart</SubmitButton>
+									</div>
+								</div>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
 		</div>
 	);
 };
